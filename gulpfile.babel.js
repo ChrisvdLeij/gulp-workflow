a031783bb86507489e0a8ea8185936947db692c4
Chris van der Leij <chris.van.der.leij@avrotros.nl>
2018-05-24 15:24:47 +0200

    adds sass linter

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,6 +4,8 @@ import gulp from 'gulp';
 import sass from 'gulp-sass';
 import autoprefixer from 'gulp-autoprefixer';
 import sourcemaps from 'gulp-sourcemaps';
+import notify from 'gulp-notify';
+import sassLint from 'gulp-sass-lint'
 
 const paths = {
   sass: './src/sass/**/*.scss',
@@ -12,17 +14,25 @@ const paths = {
   jsDest: './js/'
 };
 
-gulp.task('sass', () => {
+gulp.task('compile:sass', () => {
   return gulp.src(paths.sass)
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer())
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(paths.cssDest));
+    .pipe(gulp.dest(paths.cssDest))
+    .pipe(notify("SASS compiling done"));
+});
+
+gulp.task('lint:sass', () => {
+  return gulp.src(paths.sass)
+    .pipe(sassLint())
+    .pipe(sassLint.format())
+    .pipe(sassLint.failOnError())
 });
 
 gulp.task('watch', function(){
-  gulp.watch(paths.sass, ['sass']);
+  gulp.watch(paths.sass, ['compile:sass', 'lint:sass']);
 })
 
-gulp.task('default', ['sass', 'watch']);
+gulp.task('default', ['compile:sass', 'lint:sass', 'watch']);
